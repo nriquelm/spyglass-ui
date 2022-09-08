0056c08e78e1b7dfc64cb26af8381fdeb51d5930
Natalie Riquelmy <n.riquelmy@gmail.com>
2022-09-08 18:22:03 -0400

    styling changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
